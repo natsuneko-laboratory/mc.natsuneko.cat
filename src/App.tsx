3b13ff41171fd21c94eea51b247ae663b0ebac9b
MOCHIZUKI Natsune <me@natsuneko.cat>
2024-08-28 21:28:03 +0900

    chore: update

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,79 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  Container,
+  Footer,
+  Header,
+  Heading1,
+  Heading2,
+  Hyperlink,
+  List,
+  ListItem,
+  Section,
+} from "@natsuneko-laboratory/ui";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="grid min-h-[100dvh] grid-cols-1 grid-rows-[auto_1fr_auto] text-neutral-200">
+      <Header
+        href="/"
+        brand={{
+          title: "Floating Island MC",
+          alt: "Natsuneko Laboratory",
+          height: 36,
+          width: 36,
+          src: {
+            dark: "https://media.natsuneko.cat/logo.png",
+            light: "https://media.natsuneko.cat/logo.png",
+          },
+        }}
+      />
+      <main className="pt-6">
+        <Container>
+          <Heading1>Floating Island Minecraft Server</Heading1>
+
+          <Section>
+            <Heading2>Requirements</Heading2>
+            <List t="unordered">
+              <ListItem>Minecraft Java Edition 1.12.2</ListItem>
+              <ListItem>Minecraft Forge 1.12.2-14.23.5.2859</ListItem>
+              <ListItem>Floating Island Revived 2024</ListItem>
+            </List>
+          </Section>
+
+          <Section>
+            <Heading2>Downloads</Heading2>
+            <p>
+              Download Floating Island Revived 2024 from the following websites:
+              <List t="unordered">
+                <ListItem>
+                  <Hyperlink href="https://github.com/natsuneko-laboratory/FloatingIsland">
+                    GitHub
+                  </Hyperlink>
+                </ListItem>
+                <ListItem>
+                  <Hyperlink href="https://www.curseforge.com/minecraft/mc-mods/floating-island-revived">
+                    CurseForge
+                  </Hyperlink>
+                </ListItem>
+                <ListItem>
+                  <Hyperlink href="https://modrinth.com/mod/floating-island-revived">
+                    Modrinth
+                  </Hyperlink>
+                </ListItem>
+              </List>
+            </p>
+          </Section>
+        </Container>
+      </main>
+      <Footer
+        logo={{
+          dark: "https://media.natsuneko.cat/natsuneko-laboratory-dark.png",
+          light: "https://media.natsuneko.cat/natsuneko-laboratory-light.png",
+          height: 1217 / 2,
+          width: 359 / 2,
+        }}
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["index.html", "src/**/*.tsx"],
+  content: [
+    "index.html",
+    "src/**/*.tsx",
+    "./node_modules/@natsuneko-laboratory/ui/dist/**/*.js",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
+  darkMode: "class",
 };
